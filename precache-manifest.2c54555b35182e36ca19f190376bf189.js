845a952304ab973bbb91e9fe7ea0a49762eea40f
dzakwanzaky <dzakwanzaky48@gmail.com>
2019-04-11 08:38:45 +0700

    Updates

---
+++ precache-manifest.2c54555b35182e36ca19f190376bf189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/utsweb3/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.0bfe0192.js"
+  },
+  {
+    "revision": "021d98f8540d406799ef",
+    "url": "/utsweb3/static/js/main.5c622dac.chunk.js"
+  },
+  {
+    "revision": "169d65e6dbdd65ef96e0",
+    "url": "/utsweb3/static/js/2.d7d3ebc6.chunk.js"
+  },
+  {
+    "revision": "021d98f8540d406799ef",
+    "url": "/utsweb3/static/css/main.ebb0429c.chunk.css"
+  },
+  {
+    "revision": "7b03400471d731d9e737d70fa1997cf8",
+    "url": "/utsweb3/index.html"
+  }
+];
